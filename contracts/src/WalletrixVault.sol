101eb913648a29e41b1af45237f427b41ab6a782
ayushns01 <ayushnarayansharma@gmail.com>
2026-03-12 19:27:14 +0530

    solidity version

--- contracts/src/WalletrixVault.sol
+++ contracts/src/WalletrixVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import {BaseAccount} from "@account-abstraction/core/BaseAccount.sol";
 import {IEntryPoint} from "@account-abstraction/interfaces/IEntryPoint.sol";
